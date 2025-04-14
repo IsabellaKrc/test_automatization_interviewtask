4ac7044c61255dbab4804cdf906cb38a81638b08
Isabella Krčová <isabella.krcova@metait.cz>
2025-04-14 13:27:15 +0200

    commit first structure changes

--- Test/cypress/e2e/login.cy.js
+++ Test/cypress/e2e/login.cy.js
@@ -1,12 +1,19 @@
-<reference type="Cypress"/>
+import { loginPageElements } from "../../pages/loginpageel"
 
-describe('template spec', () => {
-  it('passes', () => {
+//<reference types="Cypress"/> 
+
+const usera = Cypress.env('username')
+const heslo = Cypress.env('password')
+
+const Login_Page = new loginPageElements
+
+describe('template spec', function()  {
+  
+  it('Log in', () => {
     cy.visit('http://localhost:3000/')
+    Login_Page.username(usera)
+    Login_Page.password(heslo)
+    Login_Page.LoginButton()
+
   })
-})
-
-it('Log in', () => {
-
-
 })
--- Test/pages/loginpageel.js
+++ Test/pages/loginpageel.js
@@ -1,15 +1,18 @@
 export class loginPageElements{
+    
+    username_text='[data-cy="username-input"]'
+    password_text='[data-cy="password-input"]'
+    login_button='[data-cy="login-button"]'
 
-
-    username(){
-
+    username(user){
+        cy.get(this.username_text).type(user)
     }
 
-    password(){
-
+    password(pass){
+        cy.get(this.password_text).type(pass) 
     }
 
     LoginButton(){
-        
+        cy.get(this.login_button).click()
     }
 }
